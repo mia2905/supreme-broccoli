74e917998c307edb566e6b9caa7785db68af389a
Holger Kretzschmar <holger.kretzschmar@check24.de>
2021-11-18 09:37:15 +0100

    build in check to reload the application lib, removed the fswatch script

--- run_macos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# start the program
-./supreme-broccoli &
-
-# watch the code folder for changes and trigger the dylib build in case the code changes
-fswatch ./code | xargs -n1 -I{} ./build_macos_application.sh
